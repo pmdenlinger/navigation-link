949617abefc2ad38ce9d579e4c36a4f1b13a6827
Paul Denlinger <pmdenlinger@icloud.com>
2021-06-21 17:50:26 -0700

    Add code

--- navigation-link/ContentView.swift
+++ navigation-link/ContentView.swift
@@ -8,14 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+    NavigationView {
+        
+//        Detail Page link...
+        NavigationLink(
+//            Detail view goes here
+            destination: Text("Detail Page"), label: {
+//                Navigation Link custom view goes here
+                Text("Go to detail page")
+            })
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
 }
